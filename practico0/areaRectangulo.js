e1e51289041d9ca8d4840490f6c718d80653eda1
Marcos Cavichioli <mcavichi@gmail.com>
2023-05-23 21:24:11 -0300

    Practico N0 finalizado

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,14 @@
+// AREA RECTANGULO
+
+let base1 = 5;
+let altura1 = 3;
+
+let area1 = base1 * altura1;
+console.log(area1); // Resultado 15
+
+let base2 = 12.5;
+let altura2 = 5.3;
+
+let area2 = base2 * altura2;
+console.log(area2); // Resultado 66.25
+
---
+++ practico0/promedio.js
@@ -0,0 +1,21 @@
+// PROMEDIO
+
+let x = 5;
+let y = 7;
+let z = 9;
+let promedio = (x + y + z)/3;
+console.log(promedio); //Resultado 7
+
+let x1 = -2;
+let y1 = -3;
+let z1 = -8;
+let promedio1 = (x1 + y1 +z1)/3;
+console.log(promedio1); //Resultado -4.33
+
+let x2 = -1.2;
+let y2 = 5.3;
+let z2 = -3.3;
+let promedio2 = (x2 + y2 + z2)/3;
+console.log(promedio2); //Resultado 0.26
+
+
