3b6d61d780c3596c8843d6ab232f70b1c61d62fb
Marcos Cavichioli <mcavichi@gmail.com>
2023-05-23 21:17:17 -0300

    Agrego resta y multiplicacion

---
+++ practico0/multiplicacion.js
@@ -0,0 +1,8 @@
+// MULTIPLICACION
+
+let num1 = 2;
+let num2 = 17;
+let num3 = 6;
+
+let resultado = num1 * num2 * num3;
+console.log(resultado); // Resultado 204
---
+++ practico0/resta.js
@@ -0,0 +1,7 @@
+// RESTA
+
+let num1 = 10;
+let num2 = 2;
+let resultado = num1 - num2;
+console.log(resultado); // Resultado 8
+
