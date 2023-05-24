190935986dcc912e9fb4205ad6dd4ae8638329fc
Marcos Cavichioli <mcavichi@gmail.com>
2023-05-23 21:20:57 -0300

    “Agrego

---
+++ practico0/conversionGrados.js
@@ -0,0 +1,15 @@
+// CONVERSION DE FAHRENHEIT A CELSIUS
+
+let fahrenheit1 = 50;
+let  conversionDeFahrenheitACelsius1 = (fahrenheit1 -32) * (5/9);
+
+console.log(conversionDeFahrenheitACelsius1); // Resultado 10 grados celsius
+
+
+let fahrenheit2 = 50;
+let  conversionDeFahrenheitACelsius2 = (fahrenheit2 -32) * (5/9);
+
+console.log(conversionDeFahrenheitACelsius2); // Resultado 23.88 grados celsius
+
+typeof conversionDeFahrenheitACelsius1;
+typeof conversionDeFahrenheitACelsius2;
